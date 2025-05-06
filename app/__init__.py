4af75306f38ccf2ec262413d186bb828a3e47ca5
yuto141592 <aricchi0208@icloud.com>
2025-05-06 15:12:23 +0900

    x

--- app/__init__.py
+++ app/__init__.py
@@ -11,15 +11,6 @@ def create_app():
     #postgres://imagepicapp_user:ZeXYZxkVbbxX7TcmiyyPLMs6EC9HL2NL@dpg-cpo5coo8fa8c73bbo7hg-a.singapore-postgres.render.com/imagepicapp
     app.config.from_object(Config)
 
-    db.init_app(app)
     app.register_blueprint(main)
-
-    with app.app_context():
-        try:
-            db.create_all()
-            print("PostgreSQL に接続成功しました！")
-        except Exception as e:
-            print(f"PostgreSQL への接続中にエラーが発生しました: {e}")
-
 
     return app
